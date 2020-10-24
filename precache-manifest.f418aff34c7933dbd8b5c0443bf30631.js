d58eaff1490edb07cf807b412144dd7b2af7e483
asong57 <asong57@naver.com>
2020-10-24 21:12:40 +0900

    Updates

---
+++ precache-manifest.f418aff34c7933dbd8b5c0443bf30631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c14dbc5c546577e478e6b3263c9cc8",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1aa146b7f8c686fc3c2e",
+    "url": "/movie_app_2020/static/css/main.7da87e8e.chunk.css"
+  },
+  {
+    "revision": "884f6d2025c1197012ad",
+    "url": "/movie_app_2020/static/js/2.28af04bf.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/movie_app_2020/static/js/2.28af04bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa146b7f8c686fc3c2e",
+    "url": "/movie_app_2020/static/js/main.546a6e13.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
